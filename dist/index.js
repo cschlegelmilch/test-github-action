acfa21d8c230755847ecf10758c8559f46ae22f4
Craig Schlegelmilch <cschlegelmilch@me.com>
2023-01-17 15:50:14 -0700

    Newe build

--- dist/index.js
+++ dist/index.js
@@ -2843,10 +2843,13 @@ async function run() {
     try {
         const ms = core.getInput('milliseconds');
         core.info(`Waiting ${ms} milliseconds ...`);
+        core.info(`Waiting ${ms} milliseconds ...`);
 
         core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
         await wait(parseInt(ms));
         core.info((new Date()).toTimeString());
+
+        core.info('Done');
 
         core.setOutput('time', new Date().toTimeString());
     } catch (error) {
